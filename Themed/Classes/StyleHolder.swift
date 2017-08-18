e0c30273ace0c4a1bf4862c9963e3791e954fec9
Brian Bal <briantbal@gmail.com>
2017-08-18 11:37:52 -0700

    Removed some commented out code

--- Themed/Classes/StyleHolder.swift
+++ Themed/Classes/StyleHolder.swift
@@ -30,26 +30,22 @@ class StyleHolder {
     }
     
     deinit {
-        // print("StyleHolder deinit")
         unregisterForStyleChanges(removeStyles: _styles)
     }
     
     public func registerForStyleChanges() {
-        // print("THEMED: registering for change notification for: \(styles)")
         for style in styles {
             NotificationCenter.default.addObserver(self, selector: #selector(onStylesChanged), name: NSNotification.Name("THEMED_STYLE_CHANGED_\(style)"), object: nil)
         }
     }
     
     public func unregisterForStyleChanges(removeStyles: [String]) {
-        // print("THEMED: unregistering for change notification for: \(removeStyles)")
         for style in removeStyles {
             NotificationCenter.default.removeObserver(self, name: NSNotification.Name("THEMED_STYLE_CHANGED_\(style)"), object: nil)
         }
     }
     
     @objc public func onStylesChanged(notif: NSNotification) {
-        // print("THEMED: recieved style changed notification for \(notif.name)")
         view?.applyStyle()
     }
     
